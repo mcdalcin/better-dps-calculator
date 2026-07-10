3af5e334184ac239b5d1b1b12e77237daa05cec8
mcdalcin <mcdalcin@users.noreply.github.com>
2026-07-10 18:10:47 -0400

    feat(equipment): define equipment slots

---
+++ src/main/java/com/dpscalc/equipment/EquipmentSlot.java
@@ -0,0 +1,15 @@
+package com.dpscalc.equipment;
+
+public enum EquipmentSlot {
+    HEAD,
+    CAPE,
+    NECK,
+    WEAPON,
+    BODY,
+    SHIELD,
+    LEGS,
+    HANDS,
+    FEET,
+    RING,
+    AMMO
+}
